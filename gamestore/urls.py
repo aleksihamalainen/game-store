073bd6b340ca24219d3de9249b860bd8db7ad317
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-01-10 18:25:34 +0200

    Edited project structure

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^games/', include('games.urls')),
+    url(r'^$', views.index, name = 'index'),
+    #url(r'^games/', include('games.urls')),
 ]
