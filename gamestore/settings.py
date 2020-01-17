196fa6c2939fc2d7b384239d390af8851f6c5c00
Markus Mantila <markus.mantila@aalto.fi>
2020-01-17 19:02:30 +0200

    Some frontend stuff

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gamestoredatabase',
-		'USER': '',
-		'PASSWORD': '',
-		'HOST': 'localhost',
-		'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
