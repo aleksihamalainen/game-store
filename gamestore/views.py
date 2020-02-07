cab47140b1e9fa2e35a4842b1f5202f298d0b6fe
Markus Mantila <markus.mantila@aalto.fi>
2020-02-07 19:49:07 +0200

    Login stuff

--- gamestore/views.py
+++ gamestore/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('login')
     return render(request, 'index.html')
