ca5722112e85d75220d95fc330fad572cea03e43
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-02-07 20:06:03 +0200

    Added a few buttons

--- games/urls.py
+++ games/urls.py
@@ -4,6 +4,6 @@ from .views import list_games, purchase_game, view_game, add_game
 urlpatterns = [
     path('games/all/', list_games, name = 'games'),
     path('games/<int:game_id>/', view_game, name = 'view_game'),
-    path('purchase/<int:game_id>/', purchase_game, name = 'purchase_game'),
+    path('games/purchase/<int:game_id>/', purchase_game, name = 'purchase_game'),
     path('add_game', add_game, name = 'add_game')
 ]
